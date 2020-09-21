917247cf69c76ddea102b92931ed3904962e6958
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-09-21 21:01:25 +0300

    config

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     MAIL_USE_TLS =True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    
+    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://oem:Jeremih23@localhost/pitches'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
