130c62fa34c33147782a06d6b123ea3699022a7b
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-09-24 20:40:09 +0300

    uri

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 export SECRET_KEY='ninjaits'
-export DATABASE_URL='postgresql+psycopg2://oem:Jeremih23@localhost/pitches'
+export DATABASE_URL='postgres+psycopg2://kmocrtyoqsvyvx:5bcedc5a205221386efa5cd29f4c605f19bb001e182c2dbadc217a096865e454@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d816a7i050f40n'
 python3.8 manage.py server
