3998267e7385980a519d5215bd444744675eb7de
Jeremiah Brian <brianjeremy123789@gmail.com>
2020-09-24 20:28:09 +0300

    change uri

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class ProdConfig(Config):
     Args:
         Config: The general configuration class with the the general configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI ='postgres+psycopg2://kmocrtyoqsvyvx:5bcedc5a205221386efa5cd29f4c605f19bb001e182c2dbadc217a096865e454@ec2-3-224-97-209.compute-1.amazonaws.com:5432/d816a7i050f40n'
+    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URI')
 class DevConfig(Config):
     '''
     development configuration subclass
